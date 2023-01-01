4b7e50ee527168aa73b94ed4cb66844b720def53
nukeTK <tarandeespsingh1809@gmail.com>
2023-01-01 17:13:16 +0530

    testing 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import query from "./queryjson";
 import {QRCodeSVG} from 'qrcode.react';
 import './App.css';
-const contractAddress = "0x435936CAC77DcF6C494CDCCAF0E5beBE76d998bb";
+//const contractAddress = "0x435936CAC77DcF6C494CDCCAF0E5beBE76d998bb";
 
 const App = () => {
-
-  //query.body.scope[0].rules.query.req.VerifiedPerson.$eq 
   return (
     <div>
       <QRCodeSVG  value={JSON.stringify(query)}/>
+     
     </div>
   )
 }
