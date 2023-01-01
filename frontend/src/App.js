2937441e332c838fc5269049f280e6977383b869
nukeTK <tarandeespsingh1809@gmail.com>
2023-01-02 02:20:34 +0530

    AirdropPolygonIDcheck

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
 import query from "./queryjson";
-import {QRCodeSVG} from 'qrcode.react';
-import './App.css';
-//const contractAddress = "0x435936CAC77DcF6C494CDCCAF0E5beBE76d998bb";
+import { QRCodeSVG } from "qrcode.react";
+import { ethers } from "ethers";
+import Button from "@mui/material/Button";
+import contractabi from "./contracts/ERC20Verifier.sol/ERC20Verifier.json";
+import { Box, Paper, Stack, Typography } from "@mui/material";
+import "./App.css";
+const contractAddress = "0x9Fbe545B16EbBaF1Af36382bF3e53Ff7D709257a";
 
 const App = () => {
-  return (
-    <div>
-      <QRCodeSVG  value={JSON.stringify(query)}/>
-     
-    </div>
-  )
-}
+  const [balance, Setbalance] = useState("");
+  const getProvider = async () => {
+    const _provider = new ethers.providers.Web3Provider(window.ethereum);
+    if (_provider) {
+      await _provider.send("eth_requestAccounts", []);
+      const _signer = _provider.getSigner();
+      const _signerAddress = await _signer.getAddress();
+      const _contract = new ethers.Contract(
+        contractAddress,
+        contractabi.abi,
+        _provider
+      );
+      const contractInstance = _contract.connect(_signer);
+      const balUser = await contractInstance.balanceOf(_signerAddress);
+      Setbalance(balUser.toString());
+    }
+  };
 
-export default App
+  return (
+    <Box>
+      <Stack gap={2} sx={{ alignItems: "center" }}>
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          Scan QR Code for the Airdrop of ERC20 Token
+        </Typography>
+        <Paper
+          sx={{
+            margin: "auto",
+            padding: "10px",
+            height: "300px",
+            width: "300px",
+          }}
+        >
+          <QRCodeSVG className="icon" value={JSON.stringify(query)} />
+        </Paper>
+        <Button
+          onClick={() => getProvider()}
+          variant="contained"
+          sx={{ width: "20%" }}
+        >
+          Click to See Your Airdrop
+        </Button>
+        <Typography variant="h6" sx={{fontWeight:"600"}}>{balance?balance:"Zero"}</Typography>
+      </Stack>
+    </Box>
+  );
+};
+
+export default App;
