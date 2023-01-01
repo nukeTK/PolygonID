70ee0900db2f43ce301127b80666369939d2fb70
nukeTK <tarandeespsingh1809@gmail.com>
2023-01-01 21:59:34 +0530

    commpleted Ageverififcation

--- frontend/src/queryjson.js
+++ frontend/src/queryjson.js
@@ -1,12 +1,12 @@
 // Corresponding QR Code
 const query = 
 {  
-    "id":"709eaa58-3eb5-4475-b56f-c0a5d0970a86",
+    "id":"2bbefd2d-dc11-4295-890d-3bdca9dd0741",
     "typ":"application/iden3comm-plain-json",
     "type":"https://iden3-communication.io/proofs/1.0/contract-invoke-request",
     "body":{
         "transaction_data":{
-            "contract_address":"0x435936CAC77DcF6C494CDCCAF0E5beBE76d998bb",  //replace it with your contract address
+            "contract_address":"0x9Fbe545B16EbBaF1Af36382bF3e53Ff7D709257a",  //replace it with your contract address
             "method_id":"b68967e2",
             "chain_id":80001,
             "network":"polygon-mumbai"
@@ -19,13 +19,13 @@ const query =
                 "query":{
                     "allowed_issuers":["*"],
                     "req":{ 
-                        "VerifiedPerson":{
-                            "$eq":1
+                        "Date":{ //query data name should be same as attributes
+                            "$lt":20020101
                             }
                         },
                     "schema":{
-                            "url":"https://s3.eu-west-1.amazonaws.com/polygonid-schemas/e79d9f55-0404-4308-b686-dee5c88e39c5.json-ld", 
-                            "type":"ProofOfPersonhood" 
+                            "url":"https://s3.eu-west-1.amazonaws.com/polygonid-schemas/c597ac41-3fc8-4510-a079-3d2e05889127.json-ld", 
+                            "type":"ERC20Verifier" //Name should smae as Schema name that you put in polygonId site
                             }
                         }
                     }
--- scripts/set-request.js
+++ scripts/set-request.js
@@ -2,20 +2,20 @@ const main = async () => {
   const circuitId = "credentialAtomicQuerySig";
   const validatorAddress = "0xb1e86C4c687B85520eF4fd2a0d14e81970a15aFB";
 
-  const schemaHash = "b4d23450cb9c8b29986e4e03c27232ed"; // extracted from PID Platform
+  const schemaHash = "aaba446f8b9ea500d96c56cabe692fe4"; // extracted from PID Platform
 
   const schemaEnd = fromLittleEndian(hexToBytes(schemaHash));
 
   const query = {
     schema: ethers.BigNumber.from(schemaEnd),
     slotIndex: 2,
-    operator: 1,
-    value: [1, ...new Array(63).fill(0).map((i) => 0)],
+    operator: 2,
+    value: [20020101, ...new Array(63).fill(0).map((i) => 0)],
     circuitId,
   };
 
   // add the address of the contract just deployed
-  ERC20VerifierAddress = "0x435936CAC77DcF6C494CDCCAF0E5beBE76d998bb";
+  ERC20VerifierAddress = "0x9Fbe545B16EbBaF1Af36382bF3e53Ff7D709257a";
 
   let erc20Verifier = await hre.ethers.getContractAt(
     "ERC20Verifier",
@@ -23,8 +23,12 @@ const main = async () => {
   );
 
   try {
-    await erc20Verifier.setZKPRequest(1, validatorAddress, query);
-    console.log("Request set");
+    const tx = await erc20Verifier.setZKPRequest(1, validatorAddress, query);
+    tx.wait();
+
+    console.log(
+      `Request set at:\nNOTE: May take a little bit to show up\nhttps://mumbai.polygonscan.com/tx/${tx.hash}`
+    );
   } catch (e) {
     console.log("error: ", e);
   }
