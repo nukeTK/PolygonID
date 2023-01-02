52d5bf0c1dbd7d9982ed3206e65c1a8e8ef06570
nukeTK <tarandeespsingh1809@gmail.com>
2023-01-02 14:41:16 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,15 @@ import { QRCodeSVG } from "qrcode.react";
 import { ethers } from "ethers";
 import Button from "@mui/material/Button";
 import contractabi from "./contracts/ERC20Verifier.sol/ERC20Verifier.json";
-import { Box, Paper, Stack, Typography } from "@mui/material";
+import { Box, Link, Paper, Stack, Typography } from "@mui/material";
 import "./App.css";
 const contractAddress = "0x9Fbe545B16EbBaF1Af36382bF3e53Ff7D709257a";
 
 const App = () => {
   const [balance, Setbalance] = useState("");
+  const [contract, setContract] = useState("");
+  const [address, setAddress] = useState("");
+  const [signer, setSigner] = useState("");
   const getProvider = async () => {
     const _provider = new ethers.providers.Web3Provider(window.ethereum);
     if (_provider) {
@@ -21,15 +24,35 @@ const App = () => {
         contractabi.abi,
         _provider
       );
-      const contractInstance = _contract.connect(_signer);
-      const balUser = await contractInstance.balanceOf(_signerAddress);
-      Setbalance(balUser.toString());
+      setContract(_contract);
+      setAddress(_signerAddress);
+      setSigner(_signer);
     }
+  };
+
+  const getBalance = async () => {
+    const contractInstance = contract.connect(signer);
+    const balUser = await contractInstance.balanceOf(address);
+    Setbalance(balUser.toString());
   };
 
   return (
     <Box>
       <Stack gap={2} sx={{ alignItems: "center" }}>
+        <Button variant="contained" onClick={() => getProvider()}>
+          {address ? address : "Connect to MetaMask wallet"}
+        </Button>
+        <Typography variant="body1">
+          Claim Your Free Airdrop Limited Slots
+        </Typography>
+        <Link
+          href="https://platform-test.polygonid.com/claim-link/33610226-a30d-430c-b99e-364330509dd7"
+          underline="none"
+          target="_blank"
+        >
+          Claim
+        </Link>
+
         <Typography variant="h6" sx={{ textAlign: "center" }}>
           Scan QR Code for the Airdrop of ERC20 Token
         </Typography>
@@ -43,14 +66,22 @@ const App = () => {
         >
           <QRCodeSVG className="icon" value={JSON.stringify(query)} />
         </Paper>
-        <Button
-          onClick={() => getProvider()}
-          variant="contained"
-          sx={{ width: "20%" }}
-        >
-          Click to See Your Airdrop
-        </Button>
-        <Typography variant="h6" sx={{fontWeight:"600"}}>{balance?balance:"Zero"}</Typography>
+        {address ? (
+          <>
+            <Button
+              onClick={() => getBalance()}
+              variant="contained"
+              sx={{ width: "20%" }}
+            >
+              Click to See Your Airdrop
+            </Button>
+            <Typography variant="h6" sx={{ fontWeight: "600" }}>
+              {balance ? balance : "Zero"}
+            </Typography>
+          </>
+        ) : (
+          "Connect Wallet"
+        )}
       </Stack>
     </Box>
   );
